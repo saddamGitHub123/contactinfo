61bd3b5d16f5fe843c08f1372e632226d4020489
saddamhosan <saddamhosan@localhost>
2021-08-10 09:08:16 +0530

    push project setup

---
+++ contactinfo/src/main/java/com/contactinfo/ContactinfoApplication.java
@@ -0,0 +1,13 @@
+package com.contactinfo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactinfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactinfoApplication.class, args);
+	}
+
+}
